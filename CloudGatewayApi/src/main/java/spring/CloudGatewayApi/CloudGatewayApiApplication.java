c5880b635dfe366cf437e6d98bd5c080c279c21b
PathakuntaVinitha <pathakuntavinnireddy123@gmail.com>
2025-08-08 21:25:44 +0530

    Gateway

---
+++ CloudGatewayApi/src/main/java/spring/CloudGatewayApi/CloudGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package spring.CloudGatewayApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApiApplication.class, args);
+	}
+
+}
